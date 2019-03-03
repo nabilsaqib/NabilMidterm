b72da2e3961e7d7ae111167fa2d9bced0d322f03
nabilsaqib <nsaqib126@gmail.com>
2019-03-03 14:35:42 -0500

    Completed Data Structures

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,11 +1,17 @@
 package datastructure;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
 
 public class DataReader {
 
 	public static void main(String[] args) {
 		/*
 		 * User API to read the below textFile and print to console.
-		 * Use BufferedReader class. 
+		 * Use BufferedReader class.
 		 * Use try....catch block to handle Exception.
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
@@ -17,11 +23,68 @@ public class DataReader {
 		 * Demonstrate how to use Stack that includes push,peek,search,pop elements.
 		 * Use For Each loop/while loop/Iterator to retrieve data.
 		 */
+		FileReader fr = null;
+		BufferedReader br = null;
+		String st = "";
+		String storedStValue = "";
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car";
 
+		try {
+			fr = new FileReader(textFile);
+			br = new BufferedReader(fr);
+		} catch (Exception e) {
+			System.out.println("File not found");
+			e.printStackTrace();
+		}
 
+		try {
+			while ((st = br.readLine()) != null) {
+				System.out.println(st);
+				storedStValue += st;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (fr != null) {
+				fr = null;
+			}
+			if (br != null) {
+				br = null;
+			}
+		}
 
+		System.out.println(" ");
+
+		String[] storeArr = storedStValue.split(" ");
+
+		Stack<String> stack = new Stack<String>();
+		List<String> list = new LinkedList<String>();
+
+		for (String vals : storeArr){
+			stack.push(vals);
+			list.add(vals);
+		}
+
+		System.out.println(" ");
+		System.out.println("Peek (Top): " + stack.peek() + " ");
+		System.out.println("Push: " + stack.push("Push") + " ");
+		System.out.println("Peek-(New Peek): " + stack.peek() + " ");
+		System.out.println("Search Tesla: " + stack.search("Tesla"));
+		System.out.println("Pop(Last In First Out): " + stack.pop());
+
+		System.out.println(" ");
+
+		System.out.println("-----FIFO order-----");
+		Iterator<String> it = list.iterator();
+		while (it.hasNext()){
+			System.out.print(it.next() + " ");
+		}
+
+		System.out.println(" ");
+		System.out.println("-----LIFO order-----");
+		while (!stack.isEmpty()) {
+			System.out.print(stack.pop() + " ");
+		}
 	}
-
 }
